85be86914596f2e79f4b56736ff4799e31f9e0a9
Michael Kahr <mikey.kahr@gmail.com>
2019-03-26 09:15:41 +0100

    added unit test

---
+++ test_password.py
@@ -0,0 +1,11 @@
+import unittest
+from pwdProducer import Password
+
+class test_Password(unittest.TestCase):
+
+    def test_check1(self):
+        self.assertTrue( Password("sickshit").check("sickshit"))
+        self.assertFalse( Password("sickshit").check("sickshit\n"))
+
+if __name__ == '__main__':
+    unittest.main()
